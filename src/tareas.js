d888d5a771882f2a703ec1023890c417165d7057
Tomas <tomascarnikian0@gmail.com>
2024-03-21 09:29:49 -0300

    otros cambios

---
+++ src/tareas.js
@@ -0,0 +1,22 @@
+class GestorTareas {
+    constructor() {
+        this.tareas = [];
+    }
+
+    agregarTarea(tarea) {
+        if (tarea.trim() !== "") {
+            // Lógica para agregar una nueva tarea
+            this.tareas.push({
+                tarea: tarea,
+                completada: false,
+            });
+            console.log("Nueva tarea agregada:", tarea);
+        } else {
+            console.error("La tarea no puede estar vacía");
+        }
+    }
+
+    obtenerTareas() {
+        return this.tareas;
+    }
+}
